fc0c0afccf80cc91f522ed91e537d46d3e5029f6
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-05 20:06:59 -0300

    header feito

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 export const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="conteiner">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} conteiner`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 export default Header;
