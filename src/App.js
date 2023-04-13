d8eaec5bd265b20b0afede65a2ce30bcb29a127a
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-12 21:00:05 -0300

    adicionando useContext ao projeto

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserContext, UserStorage } from './userContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../userContext';
 
 export const Header = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} conteiner`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
