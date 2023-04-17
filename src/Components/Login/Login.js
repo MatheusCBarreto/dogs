2b11b4810ba20f660985391087a7a9d33d337813
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-17 20:32:55 -0300

    adicionando um background de cachorrinho ao login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../useContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -14,14 +15,16 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
