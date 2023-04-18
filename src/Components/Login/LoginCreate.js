1b3bf86e9dc27fe9ad66499f5a8deb84da14fbb9
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-17 21:12:12 -0300

    adicionando a area de criação de login

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+    </section>
+  );
 };
 
 export default LoginCreate;
