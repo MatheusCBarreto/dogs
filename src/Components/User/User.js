3daa06c523c2c25c020eed5201eeb28b7d73ca96
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-21 19:40:42 -0300

    inicio da criação do header do usuario

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Route, Routes } from 'react-router-dom';
+import Feed from '../Feed/Feed';
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="conteiner">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="posta" element={<UserPhotoPost />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
