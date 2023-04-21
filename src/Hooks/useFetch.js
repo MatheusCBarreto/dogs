47ed9139a8871ce37df9538f1c2b8d2b92eb9067
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-21 13:41:43 -0300

    criando um hook chamado useFetch requesisões

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useCallback } from 'react';
+import { useState } from 'react';
+
+const useFetch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const request = useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+
+      response = await fetch(url, options);
+      json = await response.json();
+
+      if (response.ok === false) throw new Error(json.message);
+    } catch (error) {
+      json = null;
+      setError(error.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return {
+    data,
+    error,
+    loading,
+    request,
+  };
+};
+
+export default useFetch;
