07a02b44b1aa44a95b3a975fc27d57c812a4cc2e
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-04-25 21:03:43 -0300

    userHeader feito

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserHeaderNav from './UserHeaderNav';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
 
 const UserHeader = () => {
+  const [title, setTitle] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const { pathname } = location;
+
+    switch (pathname) {
+      case '/conta/postar':
+        setTitle('Poste sua foto');
+        break;
+      case '/conta/estatisticas':
+        setTitle('Estatísticas');
+        break;
+      default:
+        setTitle('Minha conta');
+        break;
+    }
+  }, [location]);
+
   return (
-    <header>
-      <h1 className="title">Título</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../useContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as Adicionar } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
+  const [mobile, setMobile] = useState(null);
+  const { userLogout } = useContext(UserContext);
+  const navegate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navegate('/login');
+  }
+
   return (
-    <nav>
-      <NavLink to="/conta">Minhas fotos</NavLink>
-      <NavLink to="/conta/estatísticas">Estatísticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar fotos</NavLink>
-      <button>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && 'Minhas fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && 'Estatísticas'}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <Adicionar />
+        {mobile && 'Adicionar fotos'}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && 'Sair'}
+      </button>
     </nav>
   );
 };
