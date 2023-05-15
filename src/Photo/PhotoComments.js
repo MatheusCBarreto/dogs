ce3b1c5726f7f350d8fbabbc1c41a39a1ef908de
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-05-15 20:24:37 -0300

    Área de coméntarios finalizada com sucesso

--- src/Photo/PhotoComments.js
+++ src/Photo/PhotoComments.js
@@ -1,15 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { UserContext } from '../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
   const [comment, setComments] = useState('');
+  const commentSection = useRef(null);
   const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comment]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentSection} className={styles.comments}>
         {comment.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}</b>
